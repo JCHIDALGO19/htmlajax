19cccf75023fb937ec99b0ef15088d76a475fe79
JCHIDALGO <jc_hidalgo_19@hotmail.com>
2018-11-28 15:39:23 -0600

    lista terminada

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 var btnActualizar= document.getElementById("btnActualizar");
+var btnGuardar= document.getElementById("btnGuardar");
 
 btnActualizar.addEventListener('click', actualizar);
+btnGuardar.addEventListener('click', guardar); 
 
-
+actualizar();
 function actualizar()
 {
 	var xhttp=new XMLHttpRequest();
@@ -39,6 +41,45 @@ function actualizar()
 			} 
 		}
 	};
+
 	xhttp.open("GET", "http://nyc.pixan.io/ajax/public/api/students", true);
 	xhttp.send();
+}
+	function guardar() 
+	{
+
+		var first_name =document.getElementById('first_name').value;
+		var last_name=document.getElementById('last_name').value;
+		var phone_number=document.getElementById('phone_number').value;
+		var email=document.getElementById('email').value;
+
+		var data=new FormData();
+				data.append('first_name', first_name);
+				data.append('last_name', first_name);
+				data.append('phone_number', phone_number);
+				data.append('email', email);
+
+		var xhttp=new XMLHttpRequest();
+		xhttp.onreadystatechange=function()
+		{
+			if(this.readyState ==4 && this.status == 200)
+			{
+				var response=JSON.parse(this.responseText);
+				if (response.status =="error")
+			{
+				alert(response.errors[0]);
+			}
+			else 
+			
+			{
+				actualizar();
+				document.getElementById('first_name').value="";
+				document.getElementById('last_name').value="";
+				document.getElementById('phone_number').value="";
+				document.getElementById('email').value="";                                                            	
+			}
+		}
+	};
+	xhttp.open("POST", "http://nyc.pixan.io/ajax/public/api/students", true);
+	xhttp.send(data);
 }
